3aaf3a74c885733f2fc997616260fd0e883763a4
halime332 <halimeyavuz1001@gmail.com>
2024-10-09 16:10:31 +0300

    JS projesi boostrap ve leaflet kütüphanesi kullanılarak tasarlandı

---
+++ README.md
@@ -0,0 +1,10 @@
+##  JS PROJESİ 
+
+-HTML CSS JS kullanılarak tasarlanmıştır
+
+-leaflet ve boostrap kütüphanesi kullanılmıştır
+
+
+- https://leafletjs.com/examples/quick-start/
+
+<img src="screen.gif"/>
---
+++ main.js
@@ -0,0 +1,131 @@
+import { detecIcon,  detecType,  setStorage } from "./helpers.js";
+
+//! HTML'den gelenler
+const form = document.querySelector("form");
+const list = document.querySelector("ul");
+//! Olay İzleyicileri
+form.addEventListener("submit", handleSubmit);
+list.addEventListener("click",handleClick);
+
+//! Ortak Kullanım Alanı
+let map;
+let coords = [];
+let notes = JSON.parse(localStorage.getItem("notes")) || [];
+let layerGroup = [];
+
+
+//* Kullanıcının konumunu öğrenme
+navigator.geolocation.getCurrentPosition(
+  loadMap,
+  console.log("Kullanıcı kabul etmedi ")
+);
+//* Haritaya tıklanınca çalışır
+function onMapClick(e){
+    form.style.display="flex";
+    coords = [e.latlng.lat, e.latlng.lng];
+    
+}
+/* Kullanıcının konumuna göre ekrana haritayı gösterme  */
+function loadMap(e){
+   
+    /* Haritanın kurulumu */
+    map= new L.map("map").setView([e.coords.latitude,e.coords.longitude],10);
+    L.control;
+    /* haritanın nasıl gözükeceğini belirler */
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    //Harita da ekrana basılacak imleçleri tutacağımız katman
+    layerGroup=L.layerGroup().addTo(map);
+    //localden gelen notları listeleme
+    renderNoteList(notes);
+   // Haritada bir tıklanma olduğunda çalışacak fonksiyon
+   map.on("click", onMapClick);
+
+}
+//ekrana marker basma
+function renderMarker(item){
+    
+    //markeri oluşturur
+    L.marker(item.coords,{icon:detecIcon(item.status)})
+    //imleçlerin olduğu katmana ekler
+    .addTo(layerGroup)
+    //üzerine tıklanınca açılacak popup ekleme
+    .bindPopup(`${item.desc}`);
+}
+
+//* form gönderildiğinde çalışır 
+function handleSubmit(e){
+    e.preventDefault() ;
+    console.log(e);
+    const desc = e.target[0].value;
+    const date = e.target[1].value;
+    const status= e.target[2].value;
+    
+    //notes dizisine eleman ekleme
+    notes.push({ id: new Date().getTime(), desc, date, status, coords});
+    console.log(notes);
+   //localStorage güncelleme
+    setStorage(notes);
+   //notları ekrana aktarabilmek için fonksiyona nots dizisini parametre olarak gönderdik
+    renderNoteList(notes);
+
+
+   //form gönderildiğinde kapanır
+    form.style.display="none";
+}
+function renderNoteList(item){
+    list.innerHTML="";
+    //markerları temizler
+    layerGroup.clearLayers();
+ 
+    item.forEach((item)=>{
+     const listElement=document.createElement("li");
+     //* datasına sahip olduğu idyi ekleme
+     listElement.dataset.id = item.id;
+     
+     listElement.innerHTML=`
+     <div>
+      <p>${item.desc}</p>
+      <p><span>Tarih:</span>${item.date}</p>
+      <p><span>Durum:</span>${detecType(item.status)}</p>
+     </div>
+     <i class="bi bi-x" id="delete"></i>
+     <i class="bi bi-airplane-fill" id="fly"></i>
+ 
+ 
+    `;
+
+    list.insertAdjacentElement("afterbegin",listElement);
+    // ekrana marker basma
+    renderMarker(item);
+ 
+    });
+ 
+}
+
+function handleClick(e){
+    console.log(e.target.id);
+    //güncellenicek elemanın id'sini öğrenme
+    const id = e.target.parentElement.dataset.id;
+    console.log(notes);
+    if(e.target.id=="delete"){
+        console.log("tıklanıldı");
+        // id2sini bldiğimiz elemanı diziden kaldırma
+        notes = notes.filter((note) => note.id != id);
+        console.log(notes);
+        
+        // localstorage güncelleme
+       setStorage(notes);
+       // ekranı güncelleme
+       renderNoteList(notes);
+
+    }  
+
+    if(e.target.id === "fly"){
+        const note = notes.find((note) => note.id ==id);
+        map.flyTo(note.coords);
+    }
+  }
